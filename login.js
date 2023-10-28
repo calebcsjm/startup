08a5ec72617f9cd2e557566e75f67bac54e549d3
Caleb Harding <calebmharding@icloud.com>
2023-10-28 12:17:07 -0600

    Add placeholder for quote API

--- login.js
+++ login.js
@@ -8,4 +8,34 @@ function login() {
     console.log(`Password: ${passwordEl.value}`)
     
     window.location.href = "goaltracker.html";
-  }
+}
+
+class QuoteOfTheDay {
+    quote;
+    source;
+
+    constructor () {
+        this.quote = this.getQuote();
+        this.source = this.getSource();
+
+        const quoteEl = document.querySelector('#loginQuote');
+        quoteEl.textContent = "\"" + this.quote + "\"";
+
+        const sourceEl = document.querySelector('#loginQuoteSource');
+        sourceEl.textContent = "- " + this.source;
+    }
+
+    getQuote() {
+        // a dummy function for now, in the future this will call an API
+        // return "A cool quote";
+        return "Remember that if you don't prioritize your life someone else will.";
+    }
+
+    getSource() {
+        // a dummy function for now, in the future this will call an API
+        // return "Someone cool";
+        return "Greg McKeown";
+    }
+}
+
+const quote = new QuoteOfTheDay();
