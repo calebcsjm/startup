4c7e4fc83586e40958ac85b49e3f5834ae2e04e3
Caleb Harding <calebmharding@icloud.com>
2023-11-18 15:54:15 -0700

    Transition to basic login structure (buggy)

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
 const DB = require('./database.js');
-
 const fs = require('fs');
+
+const authCookieName = 'token';
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
@@ -10,12 +13,78 @@ const port = process.argv.length > 2 ? process.argv[2] : 4000;
 // JSON body parsing using built-in middleware
 app.use(express.json());
 
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
+
 // Serve up the front-end static content hosting
 app.use(express.static('public'));
+
+// Trust headers that are forwarded from the proxy so we can determine IP addresses
+app.set('trust proxy', true);
 
 // Router for service endpoints
 var apiRouter = express.Router();
 app.use(`/api`, apiRouter);
+
+// CreateAuth token for a new user
+apiRouter.post('/auth/create', async (req, res) => {
+    if (await DB.getUser(req.body.username)) {
+      res.status(409).send({ msg: 'Existing user' });
+    } else {
+      const user = await DB.createUser(req.body.username, req.body.password);
+  
+      // Set the cookie
+      setAuthCookie(res, user.token);
+  
+      res.send({
+        id: user._id,
+      });
+    }
+  });
+  
+  // GetAuth token for the provided credentials
+  apiRouter.post('/auth/login', async (req, res) => {
+    const user = await DB.getUser(req.body.username);
+    if (user) {
+      if (await bcrypt.compare(req.body.password, user.password)) {
+        setAuthCookie(res, user.token);
+        res.send({ id: user._id });
+        return;
+      }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+  });
+  
+  // DeleteAuth token if stored in cookie
+  apiRouter.delete('/auth/logout', (_req, res) => {
+    res.clearCookie(authCookieName);
+    res.status(204).end();
+  });
+  
+  // GetUser returns information about a user
+  apiRouter.get('/user/:username', async (req, res) => {
+    const user = await DB.getUser(req.params.username);
+    if (user) {
+      const token = req?.cookies.token;
+      res.send({ username: user.username, authenticated: token === user.token });
+      return;
+    }
+    res.status(404).send({ msg: 'Unknown' });
+  });
+  
+  // secureApiRouter verifies credentials for endpoints
+  var secureApiRouter = express.Router();
+  apiRouter.use(secureApiRouter);
+  
+  secureApiRouter.use(async (req, res, next) => {
+    authToken = req.cookies[authCookieName];
+    const user = await DB.getUserByToken(authToken);
+    if (user) {
+      next();
+    } else {
+      res.status(401).send({ msg: 'Unauthorized' });
+    }
+  });
 
 // set habit info, return the userInfo object
 apiRouter.post('/setHabit', async (req, res) => {
@@ -53,6 +122,15 @@ apiRouter.get('/scoreboard', async (_req, res) => {
 app.use((_req, res) => {
     res.sendFile('index.html', { root: 'public' });
 });
+
+// setAuthCookie in the HTTP response
+function setAuthCookie(res, authToken) {
+    res.cookie(authCookieName, authToken, {
+      secure: true,
+      httpOnly: true,
+      sameSite: 'strict',
+    });
+  }
   
 app.listen(port, () => {    
     console.log(`Listening on port ${port}`);
--- public/login.js
+++ public/login.js
@@ -1,69 +1,73 @@
-if (localStorage.getItem("isLoggedIn")) {
-    hideLoginStuff();
-    showLogoutStuff();
-    customGreeting();
-} else {
-    showLoginStuff();
-    hideLogoutStuff();
-}
-
-function customGreeting() {
-    const greeting = document.getElementById("greeting");
-    greeting.textContent = `Welcome ${localStorage.getItem("userName")}!`;
-}
-
-function defaultGreeting() {
-    const greeting = document.getElementById("greeting");
-    greeting.textContent = "Welcome!";
-}
-
-function hideLoginStuff() {
-    const loginForm = document.getElementById("loginInfoCollector");
-    const loginInvite = document.getElementById("inviteToLogin");
-    
-    loginForm.style.display = "none";
-    loginInvite.style.display = "none";
-}
-
-function hideLogoutStuff() {
-    const logoutButton = document.getElementById("logoutButton");
-    logoutButton.style.display = "none";
-}
-
-function showLogoutStuff() {
-    const logoutButton = document.getElementById("logoutButton");
-    logoutButton.style.display = "unset";
-}
-
-function showLoginStuff() {
-    const loginForm = document.getElementById("loginInfoCollector");
-    const loginInvite = document.getElementById("inviteToLogin");
-   
-    loginForm.style.display = "unset";
-    loginInvite.style.direction = "unset";
-}
-
-function login() {
-    const nameEl = document.querySelector("#name");
-    localStorage.setItem("userName", nameEl.value);
-    const passwordEl = document.querySelector("#password");
-    localStorage.setItem("password", passwordEl.value);
-
-    console.log(`Username: ${nameEl.value}`)
-    console.log(`Password: ${passwordEl.value}`)
-
-    // set login value to true
-    localStorage.setItem("isLoggedIn", true);
-    window.location.href = "goaltracker.html";
-}
-
-function logout() {
-    localStorage.setItem("isLoggedIn", false);
-    showLoginStuff();
-    hideLogoutStuff();
-    defaultGreeting();
-    localStorage.clear();
-}
+(async () => {
+    const userName = localStorage.getItem('userName');
+    if (userName) {
+      document.querySelector('#userName').textContent = userName;
+      setDisplay('loginControls', 'none');
+      setDisplay('habitControls', 'block');
+    } else {
+      setDisplay('loginControls', 'block');
+      setDisplay('habitControls', 'none');
+    }
+  })();
+  
+  async function loginUser() {
+    loginOrCreate(`/api/auth/login`);
+  }
+  
+  async function createUser() {
+    loginOrCreate(`/api/auth/create`);
+  }
+  
+  async function loginOrCreate(endpoint) {
+    const userName = document.querySelector('#userName')?.value;
+    const password = document.querySelector('#userPassword')?.value;
+    const response = await fetch(endpoint, {
+      method: 'post',
+      body: JSON.stringify({ username: userName, password: password }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+  
+    if (response.ok) {
+      localStorage.setItem('userName', userName);
+      window.location.href = 'goaltracker.html';
+    } else {
+      const body = await response.json();
+      const modalEl = document.querySelector('#msgModal');
+      modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+      const msgModal = new bootstrap.Modal(modalEl, {});
+      msgModal.show();
+    }
+  }
+  
+  function trackHabit() {
+    window.location.href = 'goaltracker.html';
+  }
+  
+  function logout() {
+    localStorage.removeItem('userName');
+    fetch(`/api/auth/logout`, {
+      method: 'delete',
+    }).then(() => (window.location.href = '/'));
+  }
+  
+  async function getUser(username) {
+    // See if we have a user with the given username.
+    const response = await fetch(`/api/user/${username}`);
+    if (response.status === 200) {
+      return response.json();
+    }
+  
+    return null;
+  }
+  
+  function setDisplay(controlId, display) {
+    const habitControlEl = document.querySelector(`#${controlId}`);
+    if (habitControlEl) {
+      habitControlEl.style.display = display;
+    }
+  }
 
 
 function setQuote() {
