dd2ee8d45b55d0290dc2d0aa93cfaeeb130800d2
Caleb Harding <calebmharding@icloud.com>
2023-12-13 13:28:48 -0700

    React with header and footer

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,36 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+  return (
+    <body className="bg-dark text-light">
+      <header className="container-fluid">
+        <nav className="navbar fixed-top navbar-dark">
+          <a className="navbar-brand" href="#">Priorit<s>ies</s>y</a>
+          <menu className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="index.html">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="goaltracker.html">Tracker</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="scores.html">Scoreboard</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="philosophy.html">Philosophy</a>
+            </li>
+          </menu>
+        </nav>
+      </header>
+
+      <main className="container-fluid bg-secondary text-center">App components go here</main>
+
+    <footer>
+      <div className="container-fluid"> 
+        <span className="page-author">Caleb Harding</span>
+        <a className="source-code" href="https://github.com/calebcsjm/startup">GitHub</a>
+      </div>
+    </footer>
+  </body>
+  );
 }
