15afcdaa31aafef622a13415f0a6295cc6face26
Caleb Harding <calebmharding@icloud.com>
2023-12-13 21:32:31 -0700

    Implement basic login in react

--- src/app.jsx
+++ src/app.jsx
@@ -4,26 +4,36 @@ import { Login } from './login/login';
 import { GoalTracker } from './goaltracker/goaltracker';
 import { Scores } from './scores/scores';
 import { Philosophy } from './philosophy/philosophy';
+import { AuthState } from './login/authState';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
+    const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+    const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+    const [authState, setAuthState] = React.useState(currentAuthState);  
+
   return (
     <BrowserRouter>
         <div className="bg-dark text-light" id="body">
         <header className="container-fluid">
             <nav className="navbar fixed-top navbar-dark">
-            <a className="navbar-brand" href="#">Priorit<s>ies</s>y</a>
+                {authState === AuthState.Unauthenticated && (<a className="navbar-brand" href="#">Priorit<s>ies</s>y</a>)}
+                {authState === AuthState.Authenticated && (<a className="navbar-brand" href="#">{userName}'s Priorit<s>ies</s>y</a>)}
             <menu className="navbar-nav">
                 <li className="nav-item">
                     <NavLink className="nav-link" to="">Login</NavLink>
                 </li>
-                <li className="nav-item">
-                    <NavLink className="nav-link" to="goaltracker">Tracker</NavLink>
-                </li>
-                <li className="nav-item">
-                    <NavLink className="nav-link" to="scores">Scoreboard</NavLink>
-                </li>
+                {authState === AuthState.Authenticated && (
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="goaltracker">Tracker</NavLink>
+                    </li>
+                )}
+                {authState === AuthState.Authenticated && (
+                    <li className="nav-item">
+                        <NavLink className="nav-link" to="scores">Scoreboard</NavLink>
+                    </li>
+                )}
                 <li className="nav-item">
                     <NavLink className="nav-link" to="philosophy">Philosophy</NavLink>
                 </li>
@@ -33,7 +43,20 @@ export default function App() {
 
 
         <Routes>
-          <Route path='/' element={<Login /> } exact/>
+          <Route 
+            path='/' 
+            element={
+                <Login 
+                    userName={userName}
+                    authState={authState}
+                    onAuthChange={(userName, authState) => {
+                        setAuthState(authState);
+                        setUserName(userName);
+                    }}
+                />   
+            } 
+            exact
+          />
           <Route path='/goaltracker' element={<GoalTracker/>} />
           <Route path='/scores' element={<Scores />} />
           <Route path='/philosophy' element={<Philosophy />} />
@@ -53,4 +76,4 @@ export default function App() {
 
 function NotFound() {
     return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
-  }
+}
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
-export function Login() {
+import { Unauthenticated } from './unauthenticated';
+import { Authenticated } from './authenticated';
+import { AuthState } from './authState';
+
+export function Login({ userName, authState, onAuthChange }) {
   return (
     <main className='container-fluid bg-secondary text-center'>
-      <div>login displayed here</div>
+      <div>
+        {authState !== AuthState.Unknown && <h1>Welcome to Priority</h1>}
+        {authState === AuthState.Authenticated && (
+          <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
+        )}
+        {authState === AuthState.Unauthenticated && (
+          <Unauthenticated
+            userName={userName}
+            onLogin={(loginUserName) => {
+              onAuthChange(loginUserName, AuthState.Authenticated);
+            }}
+          />
+        )}
+      </div>
+      
+
     </main>
   );
+
+  // don't forget to insert the quote above, after we get to that
 }
