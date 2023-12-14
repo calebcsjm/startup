401a0402b0f35d2bcb0f41f4b148154b43377ab3
Caleb Harding <calebmharding@icloud.com>
2023-12-13 17:26:03 -0700

    Implement Philosophy in React

--- src/philosophy/philosophy.jsx
+++ src/philosophy/philosophy.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
+import './philosophy.css';
+
 export function Philosophy() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>philosophy displayed here</div>
+    <main class="container-fluid bg-secondary text-center">
+
+      <div id="picture" class="picture-box"><img src="/images/essentialism.png" alt="random" /></div>
+
+
+      <p class="attribution">Source: Image created by ReadingGraphics, drawn from ideas in "Essentialism," by Greg Mckeown </p>
+
+      <p>
+        Much is said and written about habits. We are usually trying to do too many at once. 
+      </p>
+
+      <p>
+        "The word priority came into the English language in the 1400s. It was singular. It meant the very first 
+        or prior thing. It stayed singular for the next five hundred years. Only in the 1900s did we pluralize 
+        the term and start talking about priorities." - Greg Mckeown
+      </p>
+
     </main>
   );
 }
